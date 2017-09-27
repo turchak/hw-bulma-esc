9d3bdcfc1d2817609229ba0838b2123b38c85d3f
turchak <turchaksv@gmail.com>
2017-09-27 23:03:03 +0300

    fixed css

--- app.bundle.js
+++ app.bundle.js
@@ -87,77 +87,40 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* 2 */
 /***/ (function(module, exports) {
 
-function calcAll() {
-  let inputA5 = document.querySelector('.form__input--a5');
-  let inputA4 = document.querySelector('.form__input--a4');
-  let inputA3 = document.querySelector('.form__input--a3');
-  let inputA3plus = document.querySelector('.form__input--a3plus');
-  let inputA2 = document.querySelector('.form__input--a2');
-  let allInputs = document.querySelectorAll('[data-type]');
+function runBurgerMenu() {
+  const activeClass = 'is-active';
+  const navbarBurger = document.querySelector('.navbar-burger');
+  const navbarMenu = document.querySelector('.navbar-menu');
+  navbarBurger.addEventListener('click', event => {
+    navbarBurger.classList.toggle(activeClass);
+    navbarMenu.classList.toggle(activeClass);
+  })
+};
+runBurgerMenu();
 
-  let valueInputA5 = 0;
-  let valueInputA4 = 0;
-  let valueInputA3 = 0;
-  let valueInputA3plus = 0;
-  let valueInputA2 = 0;
-// function sumAll() start
-  function sumAll() {
-    let sum = {
-      A5: valueInputA5,
-      A4: valueInputA4,
-      A3: valueInputA3,
-      A3plus: valueInputA3plus,
-      A2: valueInputA2
-    };
-    let valueAll = sum.A5 + sum.A4 + sum.A3 + sum.A3plus + sum.A2;
-    let inputSum = document.querySelector('.form__input--all');
-    console.log(sum);
-    inputSum.value = valueAll;
-  };
-// function sumAll() end
-  allInputs.forEach(elem => {
-    elem.addEventListener('change', e => {
-      e.preventDefault();
-
-      valueInputA5 = parseInt(inputA5.value);
-      valueInputA4 = parseInt(inputA4.value);
-      valueInputA3 = parseInt(inputA3.value);
-      valueInputA3plus = parseInt(inputA3plus.value);
-      valueInputA2 = parseInt(inputA2.value);
-
-      if (inputA5.value == "") {
-        inputA5.value = 0;
-        valueInputA5 = 0;
-        sumAll();
+function runModal() {
+  // [data-modal-open]
+  // [data-modal]
+  const activeClass = 'is-active';
+  const arrBottons = document.querySelectorAll('[data-modal-open], [data-modal-close] ');
+  
+  arrBottons.forEach(elem => {
+    let key = elem.dataset['modalOpen'] || elem.dataset['modalClose'];
+    //add close modal onclick close or some place
+    elem.addEventListener('click', event => {
+      document.querySelector('[data-modal=' + key + ']').classList.toggle(activeClass);
+    });
+    //add close modal on "esc"
+    document.addEventListener('keydown', event => {
+      if (event.keyCode == 27) {
+        document.querySelector('[data-modal=' + key + ']').classList.remove(activeClass);
       }
-      if (inputA4.value == "") {
-        inputA4.value = 0;
-        valueInputA4 = 0;
-        sumAll();
-      }
-
-      if (inputA3.value == "") {
-        inputA3.value = 0;
-        valueInputA3 = 0;
-        sumAll();
-      }
-      if (inputA3plus.value == "") {
-        inputA3plus.value = 0;
-        valueInputA3plus = 0;
-        sumAll();
-      }
-      if (inputA2.value == "") {
-        inputA2.value = 0;
-        valueInputA2 = 0;
-        sumAll();
-      }
-      else {
-        sumAll();
-      }
-    })
+    });
   });
-}
-calcAll();
+
+
+};
+runModal();
 
 
 /***/ })
